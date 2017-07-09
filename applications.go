f2fd49e7767c0906f6389420ab2fe0281dfa21eb
Nevio <nevio.vesic@gmail.com>
2017-07-09 23:36:31 +0200

    Syncing local machine with repo

--- applications.go
+++ applications.go
@@ -3,3 +3,86 @@
 // license that can be found in the LICENSE file.
 
 package zang
+
+import (
+	"fmt"
+
+	"github.com/zang-cloud/zang-go/helpers"
+)
+
+// ListApplications -
+func (c *Client) ListApplications(params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/Applications", c.Config.AccountSid),
+		params,
+	)
+	return
+}
+
+// GetApplication -
+func (c *Client) GetApplication(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/Applications/%s", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// CreateApplication -
+func (c *Client) CreateApplication(params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	resp, err = c.Request.Create(
+		fmt.Sprintf("%s/Applications", c.Config.AccountSid),
+		params,
+	)
+	return
+}
+
+// UpdateApplication -
+func (c *Client) UpdateApplication(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Update(
+		fmt.Sprintf("%s/Applications/%s", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// DeleteApplication -
+func (c *Client) DeleteApplication(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Delete(
+		fmt.Sprintf("%s/Applications/%s", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
---
+++ helpers/validation.go
@@ -0,0 +1,34 @@
+// Copyright 2017 The Zang Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package helpers
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ValidateAccountSid -
+func ValidateSid(sid string) error {
+	if len(sid) != 34 {
+		return fmt.Errorf("Make sure to provide valid SID. You've provided: %s", sid)
+	}
+	return nil
+}
+
+// ValidateAccountSid -
+func ValidateAccountSid(accountSid string) error {
+	if len(accountSid) != 34 || !strings.HasPrefix(accountSid, "AC") {
+		return fmt.Errorf("Make sure to provide valid AccountSid. You've provided: %s", accountSid)
+	}
+	return nil
+}
+
+// ValidateAuthToken -
+func ValidateAuthToken(authToken string) error {
+	if len(authToken) != 32 {
+		return fmt.Errorf("Make sure to provide valid AuthToken. You've provided: %s", authToken)
+	}
+	return nil
+}
