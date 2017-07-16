0eb1f6d0a19f435d80b715707801ff30af1319a5
Nevio <nevio.vesic@gmail.com>
2017-07-16 19:49:40 +0200

    Adding Sip IPAccess/Credentials List Mapping (List, Map, Delete)

---
+++ sip_mappings.go
@@ -0,0 +1,113 @@
+// Copyright 2017 The Zang Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package zang
+
+import (
+	"fmt"
+
+	"github.com/zang-cloud/zang-go/helpers"
+)
+
+// ListSipDomainCredentialMappings -
+func (c *Client) ListSipDomainCredentialMappings(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/SIP/Domains/%s/CredentialListMappings", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// MapSipDomainCredentialList -
+func (c *Client) MapSipDomainCredentialList(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Create(
+		fmt.Sprintf("%s/SIP/Domains/%s/CredentialListMappings", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// DeleteSipDomainCredentialListMapping -
+func (c *Client) DeleteSipDomainCredentialListMapping(domainSid string, sid string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Delete(
+		fmt.Sprintf("%s/SIP/Domains/%s/CredentialListMappings/%s", c.Config.AccountSid, domainSid, sid),
+		map[string]string{},
+	)
+	return
+}
+
+// ListSipDomainIpAclMappings -
+func (c *Client) ListSipDomainIpAclMappings(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/SIP/Domains/%s/IpAccessControlListMappings", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// MapSipDomainIpAcl -
+func (c *Client) MapSipDomainIpAcl(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Create(
+		fmt.Sprintf("%s/SIP/Domains/%s/IpAccessControlListMappings", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// DeleteSipDomainIpAclMapping -
+func (c *Client) DeleteSipDomainIpAclMapping(domainSid string, sid string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Delete(
+		fmt.Sprintf("%s/SIP/Domains/%s/IpAccessControlListMappings/%s", c.Config.AccountSid, domainSid, sid),
+		map[string]string{},
+	)
+	return
+}
---
+++ sip_mappings_test.go
@@ -0,0 +1,150 @@
+// Copyright 2017 The Zang Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package zang
+
+import (
+	"net/http"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestListSipDomainIpAclMappings(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("ListSipDomainIpAclMappings retrieval successful", t, func() {
+		response, err := client.ListSipDomainIpAclMappings("SD8f889084962719d55a674fd7b84f4402", map[string]string{
+			"Page":     "2",
+			"PageSize": "1",
+		})
+		So(response, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		subResource := response.GetSubResource("domains")
+
+		So(len(subResource), ShouldBeGreaterThanOrEqualTo, 0)
+
+		for transcription := range subResource {
+			So(subResource[transcription]["date_updated"], ShouldNotBeBlank)
+		}
+	})
+}
+
+func TestMapSipDomainIpAcl(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("MapSipDomainIpAcl creation successful", t, func() {
+		response, err := client.MapSipDomainIpAcl("SD8f889084962719d55a674fd7b84f4402", map[string]string{
+			"IpAccessControlListSid": "AL7c889084ddbef1bd6a6a4bceb421d4e3",
+		})
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+	})
+}
+
+func TestDeleteSipDomainIpAclMapping(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("DeleteSipDomainIpAclMapping retrieval successful", t, func() {
+		response, err := client.DeleteSipDomainIpAclMapping("SD8f889084962719d55a674fd7b84f4402", "AL7c889084ddbef1bd6a6a4bceb421d4e2")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["sid"], ShouldEqual, "AL7c889084ddbef1bd6a6a4bceb421d4e3")
+	})
+}
+
+func TestListSipDomainCredentialMappings(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("ListSipDomainCredentialMappings retrieval successful", t, func() {
+		response, err := client.ListSipDomainCredentialMappings("SD8f889084962719d55a674fd7b84f4403", map[string]string{
+			"Page":     "2",
+			"PageSize": "1",
+		})
+		So(response, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		subResource := response.GetSubResource("domains")
+
+		So(len(subResource), ShouldBeGreaterThanOrEqualTo, 0)
+
+		for transcription := range subResource {
+			So(subResource[transcription]["date_updated"], ShouldNotBeBlank)
+		}
+	})
+}
+
+func MapSipDomainCredentialList(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("MapSipDomainIpAcl creation successful", t, func() {
+		response, err := client.MapSipDomainIpAcl("SD8f889084962719d55a674fd7b84f4402", map[string]string{
+			"CredentialListSid": "SCe18890849a99187d6d454d348783d15c",
+		})
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+	})
+}
+
+func TestDeleteSipDomainCredentialListMapping(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("DeleteSipDomainCredentialListMapping retrieval successful", t, func() {
+		response, err := client.DeleteSipDomainCredentialListMapping("SD8f889084962719d55a674fd7b84f4402", "AL7c889084ddbef1bd6a6a4bceb421d4e2")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["sid"], ShouldEqual, "AL7c889084ddbef1bd6a6a4bceb421d4e3")
+	})
+}
