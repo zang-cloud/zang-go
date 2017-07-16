8fde6589035137e8705b621ce20e4b7b1b44f47c
Nevio <nevio.vesic@gmail.com>
2017-07-16 19:34:31 +0200

    Adding SipIpAccessList and SipIpAddresses tests + fixes

---
+++ sip_ipacl.go
@@ -0,0 +1,165 @@
+// Copyright 2017 The Zang Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package zang
+
+import (
+	"fmt"
+
+	"github.com/zang-cloud/zang-go/helpers"
+)
+
+// ListIpAccessLists -
+func (c *Client) ListIpAccessLists(params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists", c.Config.AccountSid),
+		params,
+	)
+	return
+}
+
+// GetIpAccessList -
+func (c *Client) GetIpAccessList(sid string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s", c.Config.AccountSid, sid),
+		map[string]string{},
+	)
+	return
+}
+
+// CreateIpAccessList -
+func (c *Client) CreateIpAccessList(params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	resp, err = c.Request.Create(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists", c.Config.AccountSid),
+		params,
+	)
+	return
+}
+
+// UpdateIpAccessList -
+func (c *Client) UpdateIpAccessList(sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Update(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s", c.Config.AccountSid, sid),
+		params,
+	)
+	return
+}
+
+// DeleteIpAccessList -
+func (c *Client) DeleteIpAccessList(sid string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Delete(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s", c.Config.AccountSid, sid),
+		map[string]string{},
+	)
+	return
+}
+
+// ListSipIpAddresses -
+func (c *Client) ListSipIpAddresses(ipAccessListSid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s/IpAddresses", c.Config.AccountSid, ipAccessListSid),
+		params,
+	)
+	return
+}
+
+// GetSipIpAddress -
+func (c *Client) GetSipIpAddress(ipAccessListSid string, sid string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Get(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s/IpAddresses/%s", c.Config.AccountSid, ipAccessListSid, sid),
+		map[string]string{},
+	)
+	return
+}
+
+// CreateSipIpAddress -
+func (c *Client) CreateSipIpAddress(ipAccessListSid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	resp, err = c.Request.Create(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s/IpAddresses", c.Config.AccountSid, ipAccessListSid),
+		params,
+	)
+	return
+}
+
+// UpdateSipIpAddresses -
+func (c *Client) UpdateSipIpAddress(ipAccessListSid string, sid string, params map[string]string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Update(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s/IpAddresses/%s", c.Config.AccountSid, ipAccessListSid, sid),
+		params,
+	)
+	return
+}
+
+// DeleteSipIpAddress -
+func (c *Client) DeleteSipIpAddress(ipAccessListSid string, sid string) (resp *Response, err error) {
+	if cerr := c.Config.Validate(); cerr != nil {
+		return nil, cerr
+	}
+
+	if verr := helpers.ValidateSid(sid); err != nil {
+		return nil, verr
+	}
+
+	resp, err = c.Request.Delete(
+		fmt.Sprintf("%s/SIP/IpAccessControlLists/%s/IpAddresses/%s", c.Config.AccountSid, ipAccessListSid, sid),
+		map[string]string{},
+	)
+	return
+}
---
+++ sip_ipacl_test.go
@@ -0,0 +1,232 @@
+// Copyright 2017 The Zang Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package zang
+
+import (
+	"net/http"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestListIpAccessLists(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("ListIpAccessLists retrieval successful", t, func() {
+		response, err := client.ListIpAccessLists(map[string]string{
+			"Page":     "1",
+			"PageSize": "1",
+		})
+		So(response, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		subResource := response.GetSubResource("ip_access_control")
+
+		So(len(subResource), ShouldBeGreaterThanOrEqualTo, 0)
+
+		for transcription := range subResource {
+			So(subResource[transcription]["date_updated"], ShouldNotBeBlank)
+		}
+	})
+}
+
+func TestGetIpAccessList(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("GetIpAccessList retrieval successful", t, func() {
+		response, err := client.GetIpAccessList("AL7c889084ddbef1bd6a6a4bceb421d4e3")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["sid"], ShouldEqual, "AL7c889084ddbef1bd6a6a4bceb421d4e3")
+	})
+}
+
+func TestCreateIpAccessList(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("CreateIpAccessList creation successful", t, func() {
+		response, err := client.CreateIpAccessList(map[string]string{
+			"FriendlyName": "Testing",
+		})
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["friendly_name"], ShouldEqual, "Testing")
+	})
+}
+
+func TestUpdateIpAccessList(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("UpdateIpAccessList retrieval successful", t, func() {
+		response, err := client.UpdateIpAccessList("AL7c889084ddbef1bd6a6a4bceb421d4e3", map[string]string{
+			"FriendlyName": "testing-update",
+		})
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["friendly_name"], ShouldEqual, "testing-update")
+	})
+}
+
+func TestDeleteIpAccessList(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("Transcription retrieval successful", t, func() {
+		response, err := client.DeleteIpAccessList("AL7c889084ddbef1bd6a6a4bceb421d4e2")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["sid"], ShouldEqual, "AL7c889084ddbef1bd6a6a4bceb421d4e3")
+	})
+}
+
+func TestListSipIpAddresses(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("ListSipIpAddresses retrieval successful", t, func() {
+		response, err := client.ListSipIpAddresses("AL7c889084ddbef1bd6a6a4bceb421d4e3", map[string]string{
+			"Page":     "1",
+			"PageSize": "1",
+		})
+		So(response, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		subResource := response.GetSubResource("credentials")
+
+		So(len(subResource), ShouldBeGreaterThanOrEqualTo, 0)
+
+		for credential := range subResource {
+			So(subResource[credential]["date_updated"], ShouldNotBeBlank)
+		}
+	})
+}
+
+func TestGetSipIpAddress(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("GetSipIpAddress retrieval successful", t, func() {
+		response, err := client.GetSipIpAddress("AL7c889084ddbef1bd6a6a4bceb421d4e3", "IP65889084cd3aec4ad39742e98491f3bd")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["sid"], ShouldEqual, "SCe18890849a99187d6d454d348783d15c")
+	})
+}
+
+func TestCreateSipIpAddress(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("CreateSipIpAddress creation successful", t, func() {
+		response, err := client.CreateSipIpAddress("AL7c889084ddbef1bd6a6a4bceb421d4e3", map[string]string{
+			"FriendlyName": "Testing",
+			"IpAddress":    "70.23.122.11",
+		})
+
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["friendly_name"], ShouldEqual, "Testing")
+	})
+}
+
+func TestUpdateSipIpAddress(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("UpdateSipIpAddress retrieval successful", t, func() {
+		response, err := client.UpdateSipIpAddress("AL7c889084ddbef1bd6a6a4bceb421d4e3", "IP65889084cd3aec4ad39742e98491f3bd", map[string]string{
+			"FriendlyName": "testing-update",
+		})
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["friendly_name"], ShouldEqual, "testing-update")
+	})
+}
+
+func TestDeleteSipIpAccess(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("DeleteSipIpAddress retrieval successful", t, func() {
+		response, err := client.DeleteSipIpAddress("AL7c889084ddbef1bd6a6a4bceb421d4e3", "IP65889084cd3aec4ad39742e98491f3bc")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+		So(response.Resource["sid"], ShouldEqual, "IP65889084cd3aec4ad39742e98491f3bd")
+	})
+}
