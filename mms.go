32ea42a8e4f256d05837790f0cdd42c5db1900a5
Nevio <nevio.vesic@gmail.com>
2017-07-16 16:31:25 +0200

    Adding MMS tests

--- mms.go
+++ mms.go
@@ -24,7 +24,7 @@ func (c *Client) ListMms(params map[string]string) (resp *Response, err error) {
 }
 
 // GetMms -
-func (c *Client) GetMms(sid string, params map[string]string) (resp *Response, err error) {
+func (c *Client) GetMms(sid string) (resp *Response, err error) {
 	if cerr := c.Config.Validate(); cerr != nil {
 		return nil, cerr
 	}
@@ -35,7 +35,7 @@ func (c *Client) GetMms(sid string, params map[string]string) (resp *Response, e
 
 	resp, err = c.Request.Get(
 		fmt.Sprintf("%s/MMS/Messages/%s", c.Config.AccountSid, sid),
-		params,
+		map[string]string{},
 	)
 	return
 }
---
+++ mms_test.go
@@ -0,0 +1,88 @@
+// Copyright 2017 The Zang Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package zang
+
+import (
+	"net/http"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestListMms(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("List mms retrieval successful", t, func() {
+		response, err := client.ListMms(map[string]string{
+			"Page":     "2",
+			"PageSize": "1",
+		})
+		So(response, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		subResource := response.GetSubResource("mms_messages")
+
+		So(len(subResource), ShouldBeGreaterThanOrEqualTo, 0)
+
+		for application := range subResource {
+			So(subResource[application]["date_updated"], ShouldNotBeBlank)
+		}
+	})
+}
+
+func TestGetMms(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("Conference retrieval successful", t, func() {
+		response, err := client.GetMms("MM24b6d24c57c94c58b3d6f35e0ca9bd71")
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		res := response.GetSingleResource("mms_row")
+		So(res["mms_sid"], ShouldEqual, "MM24b6d24c57c94c58b3d6f35e0ca9bd71")
+	})
+}
+
+func TestSendMms(t *testing.T) {
+	client, err := NewClient()
+
+	Convey("New Client With Defaults", t, func() {
+		So(client, ShouldNotBeNil)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("Send mms", t, func() {
+		response, err := client.SendMms(map[string]string{
+			"From":     "+xxx",
+			"To":       "+xxx",
+			"Body":     "body",
+			"MediaUrl": "http://full-url-to-media.jpeg",
+		})
+		So(err, ShouldBeNil)
+		So(response, ShouldNotBeNil)
+
+		So(response.Response, ShouldHaveSameTypeAs, &http.Response{})
+		So(response.Resource, ShouldNotBeNil)
+
+		res := response.GetSingleResource("mms_row")
+		So(res["mms_sid"], ShouldNotBeEmpty)
+	})
+}
