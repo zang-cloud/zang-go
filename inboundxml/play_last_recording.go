7041f2a493da5fae2681c1e8f46611a0abcb248c
Sandesh <schopdekar@avaya.com>
2019-12-31 00:43:07 +0530

    Replace Zang with Avaya in CopyRight

--- inboundxml/play_last_recording.go
+++ inboundxml/play_last_recording.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The Zang Authors. All rights reserved.
+// Copyright 2017 Avaya Cloud Inc. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
